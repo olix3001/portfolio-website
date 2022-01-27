d745815a601828fdc38ce2a0221ce4913b6b3e18
olix3001 <olimioli9@gmail.com>
2022-01-27 10:31:03 +0100

    fix: fix language on contact page

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -14,7 +14,7 @@ const Contact: NextPage = () => {
                 <div
                     className="p-6 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700 mx-4">
                     <a href="#">
-                        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Kontakt</h5>
+                        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Contact</h5>
                     </a>
                     <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
                         Email: olix3001@gmail.com<br/>
